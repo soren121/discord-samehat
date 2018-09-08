1a4da2285dd138fe4a29fa06691031a1f7132218
Nicholas Narsing <soren121@sorenstudios.com>
2018-09-08 16:05:45 -0400

    Improve meme detection; add icon by Mona

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Client, Attachment } = require('discord.js');
 const client = new Client();
 
+const re = /(same hat)(?![0-9A-Za-z_])/i;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content.toLowerCase() === 'same hat') {
+    if (re.match(msg.content) !== null) {
         const attachment = new Attachment("same_hat.png");
         msg.channel.send(attachment);
     }
